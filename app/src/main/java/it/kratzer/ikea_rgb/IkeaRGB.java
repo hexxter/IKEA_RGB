b8629917680429496d84d58afb8abae3b92bb96f
Alexander Kratzer <alexander.kratzer@semsotec.de>
2014-11-08 15:33:32 +0100

    bug at android 4.4 with BLE on screen rotation. FIX is no screen rotation enabled alway portrai view

--- app/src/main/java/it/kratzer/ikea_rgb/IkeaRGB.java
+++ app/src/main/java/it/kratzer/ikea_rgb/IkeaRGB.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,19 +32,23 @@ import it.kratzer.ikea_rgb.ble.Ble;
 import it.kratzer.ikea_rgb.ble.BleConnection;
 
 
-public class IkeaRGB extends Activity implements View.OnClickListener, BleConnection {
+public class IkeaRGB extends Activity implements View.OnClickListener, BleConnection, SeekBar.OnSeekBarChangeListener {
 
     final static String TAG = IkeaRGB.class.getSimpleName();
 
     byte[] plain;
     private Button btn;
     private TextView txt;
+    private TextView txtBrightness;
     private Spinner spinner;
+    private SeekBar brightnessSeek;
 
     private String bleName, bleDevId;
 
     private Context appcontext;
     private Ble ble;
+
+    private int brightness = 255;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +70,13 @@ public class IkeaRGB extends Activity implements View.OnClickListener, BleConnec
         btn.setClickable( false );
 
         txt = (TextView) findViewById(R.id.textView);
+        txtBrightness = (TextView) findViewById(R.id.textView2);
+        txtBrightness.setText(getString(R.string.brightness) + ": 100%");
+
+        brightnessSeek = (SeekBar) findViewById(R.id.seekBar);
+        brightnessSeek.setOnSeekBarChangeListener(this);
+        brightnessSeek.setMax(255);
+        brightnessSeek.setProgress(255);
 
         spinner = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.programs, android.R.layout.simple_spinner_item);
@@ -115,15 +128,15 @@ public class IkeaRGB extends Activity implements View.OnClickListener, BleConnec
 
     public static byte[] escape(byte[] block){
         ByteBuffer bbuf = ByteBuffer.allocate( block.length*2 );
-        for( byte n : block ){
-            if( n == 0x00 ){
-                bbuf.put( (byte)0xFF );
-                bbuf.put( (byte)0x00 );
-            }else if( n == (byte)0xFF ){
-                bbuf.put( (byte)0xFF );
-                bbuf.put( (byte)0x55 );
-            }else{
-                bbuf.put( n );
+        for (byte n : block) {
+            if (n == 0x00) {
+                bbuf.put((byte) 0xFF);
+                bbuf.put((byte) 0x00);
+            } else if (n == (byte) 0xFF) {
+                bbuf.put((byte) 0xFF);
+                bbuf.put((byte) 0x55);
+            } else {
+                bbuf.put(n);
             }
         }
         //EOF
@@ -135,23 +148,40 @@ public class IkeaRGB extends Activity implements View.OnClickListener, BleConnec
         return result;
     }
 
-    private byte[] buildRandomFrame( byte brightness ){
-
+    private void addSecurPass(ArrayList<Byte> list) {
         byte[] token = new byte[1];
         try {
             token = secToken();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        Log.i("it.kratzer.ikea_rgb", String.format("B: %02X", brightness) );
         Log.i("it.kratzer.ikea_rgb", "T: "+AES.getHexString( token ) );
+        for (byte b : token) {
+            list.add(new Byte(b));
+        }
+    }
+
+    private byte[] finishFrame(ArrayList<Byte> list) {
+        byte[] complete = new byte[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            complete[i] = list.get(i).byteValue();
+        }
+        byte[] frame = escape(complete);
+        Log.i("it.kratzer.ikea_rgb", "C: " + AES.getHexString(frame));
+        return frame;
+    }
+
+    private byte[] buildRandomFrame(byte brightness) {
+
+        Log.i("it.kratzer.ikea_rgb", String.format("P: Random B: %02X", brightness));
 
         ArrayList<Byte> alist = new ArrayList<Byte>();
+
+        alist.add((byte) 0x03);
         alist.add( brightness );
-        for(byte b : token) {
-            alist.add(new Byte(b));
-        }
+
+        addSecurPass(alist);
+
         SecureRandom sr = new SecureRandom();
         byte[] rand = new byte[3];
         for( int i = 0; i<10; i++ ){
@@ -162,30 +192,77 @@ public class IkeaRGB extends Activity implements View.OnClickListener, BleConnec
                 alist.add( rand[2] );
             }
         }
-        byte[] complete = new byte[ alist.size() ];
-        for( int i = 0; i< alist.size(); i++ ){
-            complete[i] = alist.get(i).byteValue();
+
+        return finishFrame(alist);
+    }
+
+    private byte[] offFrame(byte brightness) {
+
+        Log.i("it.kratzer.ikea_rgb", "P: Off ");
+
+        ArrayList<Byte> alist = new ArrayList<Byte>();
+
+        alist.add((byte) 0x00);
+        alist.add(brightness);
+
+        addSecurPass(alist);
+        alist.add((byte) 0x55); // fill byte (send the EOF alone in a transfer package is not working)
+
+        return finishFrame(alist);
+    }
+
+    private byte[] rainbowFrame(byte brightness) {
+
+        Log.i("it.kratzer.ikea_rgb", String.format("P: Rainbow B: %02X", brightness));
+
+        ArrayList<Byte> alist = new ArrayList<Byte>();
+
+        alist.add((byte) 0x01);
+        alist.add(brightness);
+
+        addSecurPass(alist);
+        alist.add((byte) 0x55); // fill byte (send the EOF alone in a transfer package is not working)
+
+        return finishFrame(alist);
+    }
+
+    private byte[] tickFrame(byte brightness) {
+
+        Log.i("it.kratzer.ikea_rgb", String.format("P: Tick B: %02X", brightness));
+
+        ArrayList<Byte> alist = new ArrayList<Byte>();
+
+        alist.add((byte) 0x02);
+        alist.add(brightness);
+
+        addSecurPass(alist);
+
+        SecureRandom sr = new SecureRandom();
+        byte[] rand = new byte[3];
+
+        sr.nextBytes(rand);
+        for (int n = 0; n < 3; n++) {
+            alist.add(rand[0]);
+            alist.add(rand[1]);
+            alist.add(rand[2]);
         }
 
-        byte[] frame = escape( complete );
-        Log.i("it.kratzer.ikea_rgb", "C: "+AES.getHexString( frame ) );
-        return frame;
+        return finishFrame(alist);
     }
 
     @Override
     public void onClick(View v) {
         Log.i("it.kratzer.ikea_rgb", "klick ");
-        byte prog = 0;
+
         if( spinner.getSelectedItem().toString().contentEquals( "Off" ) ){
-            prog = 0;
+            sendFrame(offFrame((byte) this.brightness));
         }else if( spinner.getSelectedItem().toString().contentEquals( "Rainbow" ) ){
-            prog = 1;
+            sendFrame(rainbowFrame((byte) this.brightness));
         }else if( spinner.getSelectedItem().toString().contentEquals( "Tick" ) ){
-            prog = 2;
+            sendFrame(tickFrame((byte) this.brightness));
         }else if( spinner.getSelectedItem().toString().contentEquals( "Random" ) ){
-            prog = (byte)0xFF;
+            sendFrame(buildRandomFrame((byte) this.brightness));
         }
-        sendFrame(buildRandomFrame( prog ) );
     }
 
     private void sendFrame( byte[] frame ){
@@ -226,17 +303,23 @@ public class IkeaRGB extends Activity implements View.OnClickListener, BleConnec
     @Override
     protected void onStop() {
         super.onStop();
-        if (ble.getGatt() != null) {
-            // For better reliability be careful to disconnect and close the connection.
-            ble.getGatt().disconnect();
-            ble.getGatt().close();
-        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        if (ble.getGatt() != null) {
+            // For better reliability be careful to disconnect and close the connection.
+            ble.getGatt().disconnect();
+            Handler mHandler = new Handler();
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    ble.getGatt().close();
+                }
+            }, 10);
 
+        }
         System.exit(0);
     }
 
@@ -266,5 +349,20 @@ public class IkeaRGB extends Activity implements View.OnClickListener, BleConnec
                 txt.setText( getString(R.string.nconnect) );
             }
         });
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        this.brightness = progress;
+        Log.i(TAG, "brightness changed: " + ((int) (progress / 255.0 * 100.0)) + "%");
+        this.txtBrightness.setText(getString(R.string.brightness) + ": " + ((int) (progress / 255.0 * 100.0)) + "%");
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
     }
 }
